be05819056a0e1f397b6c24ac52c13774bd3b247
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2024-06-09 23:54:03 +0800

    feat: animation between register and login

--- components/Form/authForm.tsx
+++ components/Form/authForm.tsx
@@ -1,15 +1,22 @@
+import { ChangeEvent } from "react";
+
 interface prop {
     title?: string;
     description?: string;
     type?: string;
+    value?: string;
+    onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
 function Item(props: prop) {
-    const { title, description, type = "text" } = props;
+    const { title, description, type = "text", value, onChange } = props;
     return (
         <div className="my-4">
             {title && <span>{title}：</span>}
             <input
+                required
+                value={value}
+                onChange={onChange}
                 type={type}
                 placeholder={description}
                 className="p-3 bg-gray-100 hover:bg-white transition-all duration-300 px-5 w-full rounded-lg focus-visible:outline-none"
--- pages/auth/login/+Page.tsx
+++ pages/auth/login/+Page.tsx
@@ -1,31 +1,152 @@
+import { useState } from "react";
 import AuthForm from "../../../components/Form/authForm";
 import xiaoXuQiu from "/xiaoXuQiu.jpg";
+import axios, { AxiosError } from "axios";
+import "./animation.css";
+import { message } from "antd";
 
 export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const [toNext, setToNext] = useState(0);
+    const [toNextAnimeMiddle, setToNextAnimeMiddle] = useState(false);
+
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const toAnother = () => {
+        if (toNext !== 2) {
+            setToNext(2);
+            setTimeout(() => {
+                setToNextAnimeMiddle(true);
+            }, 1000);
+        } else {
+            setToNext(1);
+            setTimeout(() => {
+                setToNextAnimeMiddle(false);
+            }, 1000);
+        }
+    };
+
+    const submitHandler = async () => {
+        if (!toNextAnimeMiddle) {
+            //登录
+            if (username.trim().length < 4 || password.trim().length < 8) {
+                messageApi.open({
+                    type: "warning",
+                    content: "请输入正确的用户名和密码",
+                });
+            } else
+                try {
+                    const res = await axios.post(import.meta.env.BASE_URL + "/auth", {
+                        name: username,
+                        password,
+                    });
+                    console.log(res.data);
+                } catch (error) {
+                    if (axios.isAxiosError(error)) {
+                        if (error.response?.status === 401)
+                            messageApi.open({
+                                type: "warning",
+                                content: "用户名或密码错误",
+                            });
+                        else {
+                            messageApi.open({
+                                type: "warning",
+                                content: "服务器错误",
+                            });
+                            console.error(error);
+                            throw new AxiosError();
+                        }
+                    }
+                }
+        } else {
+            //注册
+            if (
+                username.trim().length < 4 ||
+                password.trim().length < 8 ||
+                confirmPassword.trim().length < 8 ||
+                confirmPassword !== password
+            ) {
+                messageApi.open({
+                    type: "warning",
+                    content: "请输入正确的用户名和密码",
+                });
+            } else
+                try {
+                    const res = await axios.post(import.meta.env.BASE_URL + "/auth/reg", {
+                        name: username,
+                        password,
+                    });
+                    console.log(res.data);
+                } catch (error) {
+                    if (axios.isAxiosError(error)) {
+                        if (error.response?.status === 400)
+                            messageApi.open({
+                                type: "warning",
+                                content: "用户名已存在",
+                            });
+                        else {
+                            messageApi.open({
+                                type: "warning",
+                                content: "服务器错误",
+                            });
+                            console.error(error);
+                            throw new AxiosError();
+                        }
+                    }
+                }
+        }
+    };
+
     return (
-        <div className="mb-32 md:mb-0 rounded-2xl w-10/12 md:w-6/12 shadow-2xl overflow-hidden flex flex-col md:flex-row m-0">
-            <img src={xiaoXuQiu} className="w-full h-56 md:h-auto object-cover object-bottom md:w-1/2" />
-            <div className="bg-gray-200 w-full p-8 px-0">
-                <h1 className="font-serif text-2xl text-center">Sign in</h1>
-                <form className="mt-8 items-center flex flex-col">
-                    <AuthForm.Item description="请输入用户名"></AuthForm.Item>
-                    <AuthForm.Item description="请输入密码"></AuthForm.Item>
-                    <div className="my-4 flex flex-row">
-                        <input
-                            type="button"
-                            value="注册"
-                            className={`mx-1 bg-zinc-100 hover:bg-gray-50 active:bg-gray-200 transition-all
+        <>
+            {contextHolder}
+            <div
+                className={`mb-32 md:mb-0 rounded-2xl w-10/12 md:w-6/12 shadow-2xl overflow-hidden normal-1
+            flex flex-col ${toNextAnimeMiddle ? "md:flex-row-reverse" : "md:flex-row"} m-0 ${toNext !== 0 && (toNext === 2 ? "rotate-next" : "rotate-prev")}`}>
+                <img src={xiaoXuQiu} className="w-full h-56 md:h-auto object-cover object-bottom md:w-1/2" />
+                <div className="bg-gray-200 w-full p-8 px-0">
+                    <h1 className="font-serif text-2xl text-center">{toNextAnimeMiddle ? "Sign Up" : "Sign In"}</h1>
+                    <form
+                        className="m-8 items-center flex flex-col"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            submitHandler();
+                        }}>
+                        <AuthForm.Item
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            description="请输入用户名"></AuthForm.Item>
+                        <AuthForm.Item
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            description="请输入密码"></AuthForm.Item>
+                        {toNextAnimeMiddle && (
+                            <AuthForm.Item
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                description="请输入确认密码"></AuthForm.Item>
+                        )}
+                        <div className="my-4 flex flex-row">
+                            <input
+                                type="button"
+                                value={toNextAnimeMiddle ? "登录" : "注册"}
+                                onClick={toAnother}
+                                className={`mx-1 bg-zinc-100 hover:bg-gray-50 active:bg-gray-200 transition-all
                                 cursor-pointer text-black p-2 px-5 w-full rounded-lg border-gray-300 border-solid border active:ani-wave-blue`}
-                        />
-                        <input
-                            type="submit"
-                            value="登录"
-                            className={`mx-1 bg-blue-500 hover:bg-blue-400 active:bg-blue-600 transition-all
+                            />
+                            <input
+                                type="submit"
+                                value={toNextAnimeMiddle ? "注册" : "登录"}
+                                className={`mx-1 bg-blue-500 hover:bg-blue-400 active:bg-blue-600 transition-all
                                 cursor-pointer text-white p-2 px-5 w-full rounded-lg active:ani-wave-blue`}
-                        />
-                    </div>
-                </form>
+                            />
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
