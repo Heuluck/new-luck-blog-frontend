72548390a704027cf92cec3e1ca22f7f871eda3c
Heuluck Lu <89375068+Heuluck@users.noreply.github.com>
2024-06-13 23:05:43 +0800

    feat: 受够回调地狱了，新的Promise查询语句

--- server/database/connection.ts
+++ server/database/connection.ts
@@ -1,5 +1,6 @@
 import { FieldPacket, QueryError, QueryResult } from "mysql2";
 import pool from "./database";
+import { SQLError } from "@server/common/Errors";
 
 export default async function dbQuery(
     query: string,
@@ -22,6 +23,28 @@ export default async function dbQuery(
             }
             onSuccess(results, fields);
             con.release();
+        });
+    });
+}
+
+export function dQuery(query: string, params: any[]): Promise<{ results: QueryResult; fields: FieldPacket[] }> {
+    return new Promise((resolve, rejects) => {
+        pool.getConnection((err, con) => {
+            if (err) {
+                console.log("\x1B[31m%s\x1B[0m", "[SQL ERROR]", err.message);
+                rejects(new SQLError(err.message));
+                con.release();
+                return;
+            }
+            con.query(query, params, function (err, results, fields) {
+                if (err) {
+                    console.log("\x1B[31m%s\x1B[0m", "[SQL ERROR]", err.message);
+                    rejects(new SQLError(err.message));
+                    return;
+                }
+                resolve({ results, fields });
+                con.release();
+            });
         });
     });
 }
